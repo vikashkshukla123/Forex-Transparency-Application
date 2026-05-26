012901f990ce643980d314d02d040c3f446456c2
sinnu2004 <152984546+sinnu2004@users.noreply.github.com>
2026-05-26 17:10:12 +0530

    main.jsx fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,34 +1,28 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
-
-// import { AuthProvider } from "./context/AuthContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <AuthProvider>
-//       <App />
-//     </AuthProvider>
-//   </React.StrictMode>
-// );
-
-
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
 
 import "./index.css";
 
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white text-5xl">
-      Frontend Working Successfully
-    </div>
-  );
-}
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+
+      <AuthProvider>
+
+        <App />
+
+      </AuthProvider>
+
+    </BrowserRouter>
+
   </React.StrictMode>
 );
