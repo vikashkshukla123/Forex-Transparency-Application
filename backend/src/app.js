4d947889d19bc8dce9bd26186999fca7c870c0ac
sinnu2004 <152984546+sinnu2004@users.noreply.github.com>
2026-05-26 15:27:50 +0530

    deploy test

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default api;
+
 const express = require('express');
 const cors = require('cors');
 
